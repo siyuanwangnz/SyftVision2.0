6fd1d890c1a1d7c132b0b17ac31e8bc1b37a6103
Steve.Wang <lmnwsy@gmail.com>
2022-04-06 00:05:12 +1200

    add get scan list method

--- SyftVision/Public/Instrument/ScanFile.cs
+++ SyftVision/Public/Instrument/ScanFile.cs
@@ -2,13 +2,39 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Public.Instrument
 {
     public class ScanFile
     {
-        public string Name { get; set; }
-        public string InstruFolder { get; set; }
+        public ScanFile(string file, string remoteFolder)
+        {
+            File = file;
+
+            RemoteFolder = remoteFolder;
+
+            Name = Regex.Match(File, @"^(.*)-\d+-\d{8}-\d{6}\.xml$").Groups[1].Value;
+
+            NameCode = Name.ToLower().Replace(" ", "");
+
+            string id = Regex.Match(File, @"-(\d+)-\d{8}-\d{6}\.xml$").Groups[1].Value;
+            ID = int.Parse(id == "" ? "0" : id);
+
+            Date_Time = Regex.Match(File, @"-(\d{8}-\d{6})\.xml$").Groups[1].Value;
+
+            Date = Regex.Match(Date_Time, @"^(\d{8})-\d{6}$").Groups[1].Value;
+
+            Time = Regex.Match(Date_Time, @"^\d{8}-(\d{6})$").Groups[1].Value;
+        }
+        public string File { get; }
+        public string RemoteFolder { get; }
+        public string Name { get; }
+        public string NameCode { get; }
+        public int ID { get; }
+        public string Date_Time { get; }
+        public string Date { get; }
+        public string Time { get; }
     }
 }
--- SyftVision/Public/SFTP/InstrumentServer.cs
+++ SyftVision/Public/SFTP/InstrumentServer.cs
@@ -10,16 +10,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
+using System.Text.RegularExpressions;
 
 namespace Public.SFTP
 {
     public class InstrumentServer : SFTPServices
     {
-        private readonly string RemoteMethodsPath = "/usr/local/syft/lib/methods/";
+        private readonly string RemoteMethodPath = "/usr/local/syft/lib/methods/";
 
         private readonly string LocalBatchPath = "./Temp/Batch/";
         private readonly string LocalBatchTempFile = "BatchTemp.xml";
         private string LocalBatchTempFilePath => LocalBatchPath + LocalBatchTempFile;
+
+        private readonly string RemoteScanPath = "/usr/local/syft/data/";
+
         public InstrumentServer(string ipAddress, Options options) : base(ipAddress, options.Port, options.User, options.Password)
         {
             // Check local directory
@@ -31,7 +35,7 @@ namespace Public.SFTP
             try
             {
                 Connect();
-                List<string> files = GetFileList(RemoteMethodsPath, "sba");
+                List<string> files = GetFileList(RemoteMethodPath, "sba");
                 Disconnect();
                 return new ObservableCollection<string>(files);
             }
@@ -50,10 +54,53 @@ namespace Public.SFTP
             try
             {
                 Connect();
-                DownloadFile(RemoteMethodsPath + batchFile, LocalBatchTempFilePath);
+                DownloadFile(RemoteMethodPath + batchFile, LocalBatchTempFilePath);
                 Disconnect();
 
                 return new BatchFile(XElement.Load(LocalBatchTempFilePath)).GetMethodList();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                Disconnect();
+            }
+
+        }
+
+        public List<ScanFile> GetScanFileList(DateTime date, DateTime time)
+        {
+            try
+            {
+                // Get date and time
+                string _date = date.ToString("yyyy-MM-dd");
+                string _time = time.ToString("HHmmss");
+
+                Connect();
+
+                // Get folder list that folder's format is xxxx-xx-xx and >= date
+                List<string> folderList = GetDirectoryList(RemoteScanPath);
+                folderList = folderList.Where(x => Regex.IsMatch(x, @"^\d{4}-\d{2}-\d{2}$") && string.CompareOrdinal(x, _date) >= 0).ToList();
+
+                // Get scan list that >= time
+                List<ScanFile> scanFileList = new List<ScanFile>();
+                foreach (string folder in folderList)
+                {
+                    List<string> fileList = GetFileList(RemoteScanPath + folder, "xml");
+                    foreach (var file in fileList)
+                        scanFileList.Add(new ScanFile(file, folder));
+                }
+
+                Disconnect();
+
+                scanFileList = scanFileList.Where(x => string.CompareOrdinal(x.Time, _time) >= 0).ToList();
+
+                // Order scan list (ascending comparing by Date Time and then ID)
+                scanFileList.Sort((a, b) => a.Date_Time.CompareTo(b.Date_Time) == 0 ? a.ID.CompareTo(b.ID) : a.Date_Time.CompareTo(b.Date_Time));
+
+                return scanFileList;
             }
             catch (Exception)
             {
