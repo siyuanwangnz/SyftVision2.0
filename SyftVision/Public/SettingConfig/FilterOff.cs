1acb8581eb3acbde20f97d00d12736f1be4f8f61
Steve.Wang <lmnwsy@gmail.com>
2022-05-12 12:56:33 +1200

    add filter off list

---
+++ SyftVision/Public/SettingConfig/FilterOff.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Public.SettingConfig
+{
+    public class FilterOff
+    {
+        public string Wildcard { get; set; } = "";
+        public string Regular => "^" + Regex.Escape(Wildcard).Replace("\\?", ".").Replace("\\*", ".*") + "$";
+    }
+}
--- SyftVision/SettingCofig/ViewModels/SettingConfigViewModel.cs
+++ SyftVision/SettingCofig/ViewModels/SettingConfigViewModel.cs
@@ -4,6 +4,7 @@ using Prism.Mvvm;
 using Prism.Regions;
 using Prism.Services.Dialogs;
 using Public.ChartConfig;
+using Public.SettingConfig;
 using Public.SFTP;
 using Public.TreeList;
 using System;
@@ -27,6 +28,9 @@ namespace SettingConfig.ViewModels
             _eventAggregator = eventAggregator;
             _dialogService = dialogService;
             _syftServer = new SyftServer();
+            #region Test
+            FilterOffList = new ObservableCollection<FilterOff>() { new FilterOff(), new FilterOff() };
+            #endregion
         }
 
         #region Toolbar
@@ -52,12 +56,7 @@ namespace SettingConfig.ViewModels
 
                                 ChartProp chartProp = _syftServer.DownloadChart(treeNode);
 
-                                SelectedChartType = chartProp.ChartType;
-                                Tittle = chartProp.Tittle;
-                                SubTittle = chartProp.SubTittle;
-                                SelectedExpectedRange = chartProp.ExpectedRange;
-                                SelectedPhase = chartProp.Phase;
-                                ComponentList = chartProp.ComponentList;
+
                             }
                         });
                     }
@@ -74,17 +73,10 @@ namespace SettingConfig.ViewModels
             {
                 return new DelegateCommand(() =>
                 {
-                    if (SelectedChartType == null || Tittle == null || Tittle == "" || SubTittle == null || SubTittle == "")
-                    {
-                        MessageBox.Show($"Chart Type, Tittle, Sub-Tittle can not be empty", "WARNING", MessageBoxButton.OK, MessageBoxImage.Warning);
-                        return;
-                    }
-
-                    ChartProp chartProp = new ChartProp(SelectedChartType, Tittle, SubTittle, SelectedExpectedRange, SelectedPhase, ComponentList);
 
                     try
                     {
-                        _syftServer.UploadChart(chartProp);
+
                     }
                     catch (Exception ex)
                     {
@@ -99,13 +91,7 @@ namespace SettingConfig.ViewModels
             {
                 return new DelegateCommand(() =>
                 {
-                    if (SelectedChartType == null)
-                    {
-                        MessageBox.Show($"Chart Type can not be empty", "WARNING", MessageBoxButton.OK, MessageBoxImage.Warning);
-                        return;
-                    }
-                    ComponentList = new ObservableCollection<Component> { };
-                    ComponentList.Add(SelectedChartType.Component.Copy());
+
                 });
             }
         }
@@ -121,79 +107,30 @@ namespace SettingConfig.ViewModels
             get => _subTittle;
             set => SetProperty(ref _subTittle, value);
         }
-        private ObservableCollection<ChartType> _chartTypeList = ChartType.ReferList;
-        public ObservableCollection<ChartType> ChartTypeList
-        {
-            get => _chartTypeList;
-            set => SetProperty(ref _chartTypeList, value);
-        }
-        private ChartType _selectedChartType;
-        public ChartType SelectedChartType
-        {
-            get => _selectedChartType;
-            set => SetProperty(ref _selectedChartType, value);
-        }
-        private ObservableCollection<string> _expectedRangeList = new ObservableCollection<string>() { "Upper Limit", "Under Limit" };
-        public ObservableCollection<string> ExpectedRangeList
-        {
-            get => _expectedRangeList;
-            set => SetProperty(ref _expectedRangeList, value);
-        }
-        private string _selectedExpectedRange = "Upper Limit";
-        public string SelectedExpectedRange
-        {
-            get => _selectedExpectedRange;
-            set => SetProperty(ref _selectedExpectedRange, value);
-        }
-        private ObservableCollection<string> _phaseList = new ObservableCollection<string>() { "All", "Preparation", "Background", "Sample" };
-        public ObservableCollection<string> PhaseList
-        {
-            get => _phaseList;
-            set => SetProperty(ref _phaseList, value);
-        }
-
-        private string _selectedPhase = "All";
-        public string SelectedPhase
-        {
-            get => _selectedPhase;
-            set => SetProperty(ref _selectedPhase, value);
-        }
         #endregion
 
-        #region Component list
-        private ObservableCollection<Component> _componentList;
-        public ObservableCollection<Component> ComponentList
+        private ObservableCollection<FilterOff> _filterOffList;
+        public ObservableCollection<FilterOff> FilterOffList
         {
-            get => _componentList;
-            set => SetProperty(ref _componentList, value);
+            get => _filterOffList;
+            set => SetProperty(ref _filterOffList, value);
         }
-        private Component _selectedComponent;
-        public Component SelectedComponent
+        private FilterOff _selectedFilterOff;
+        public FilterOff SelectedFilterOff
         {
-            get => _selectedComponent;
-            set => SetProperty(ref _selectedComponent, value);
+            get => _selectedFilterOff;
+            set => SetProperty(ref _selectedFilterOff, value);
         }
-        public DelegateCommand AddUpCommand
+        public DelegateCommand AddCommand
         {
             get
             {
                 return new DelegateCommand(() =>
                 {
-                    ComponentList.Insert(ComponentList.IndexOf(SelectedComponent), SelectedChartType.Component.Copy());
+                    FilterOffList.Insert(FilterOffList.IndexOf(SelectedFilterOff) + 1, new FilterOff());
                 });
             }
         }
-        public DelegateCommand AddDownCommand
-        {
-            get
-            {
-                return new DelegateCommand(() =>
-                {
-                    ComponentList.Insert(ComponentList.IndexOf(SelectedComponent) + 1, SelectedChartType.Component.Copy());
-                });
-            }
-        }
-        #endregion
 
     }
 }
