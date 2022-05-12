78700704a5a7274815c19f3715919b582e58299c
Steve.Wang <lmnwsy@gmail.com>
2022-05-12 17:06:06 +1200

    next to setting

---
+++ SyftVision/Public/SettingConfig/Setting.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Public.SettingConfig
+{
+    public class Setting
+    {
+        public enum Type { Text, Value, OnOff, Table, Map }
+        public string Name { get; set; } = "";
+        public string Content { get; set; } = "";
+        public Type TypeSet { get; set; } = Type.Text;
+    }
+}
